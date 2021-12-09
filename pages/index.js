78ce8ad1db938bdc2b1889a070bcde3544a3140e
CnftTrash <95837301+CnftTrash@users.noreply.github.com>
2021-12-08 22:55:50 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Cnft Trash!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <Header title="Coming Soon" />
+        <p className="picture">
+    
         </p>
       </main>
 
