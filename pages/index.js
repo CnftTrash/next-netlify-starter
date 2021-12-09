996d1ab9d1540dcff8a35056d921dccdbfc785b2
CnftTrash <95837301+CnftTrash@users.noreply.github.com>
2021-12-08 23:23:31 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,24 @@ export default function Home() {
       <main>
         <Header title="Coming Soon" />
     <body>
-       
-    <img src "https://raw.githubusercontent.com/CnftTrash/next-netlify-starter/main/trash-lord.png">
+       import Image from 'next/image'
+
+const myLoader = ({ src, width, quality }) => {
+  return `https://example.com/${src}?w=${width}&q=${quality || 75}`
+}
+
+const MyImage = (props) => {
+  return (
+    <Image
+      loader={myLoader}
+      src="https://raw.githubusercontent.com/CnftTrash/next-netlify-starter/main/trash-lord.png"
+      alt="Picture of the author"
+      width={500}
+      height={500}
+    />
+  )
+}
+  
         
       </body>
       </main>
